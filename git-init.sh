7a55325bcbc7db5d4c57e81ceab31fef2fde8c85
benw <ben@bwhouse.net>
2021-10-01 00:45:58 +1000

    moved traefik config to anisble playbook

--- git-init.sh
+++ git-init.sh
@@ -19,7 +19,7 @@ done
 if ( git show :vars/vault.yml | grep -q "$ANSIBLE_VAULT;" ); then
 echo "[38;5;108mVault Encrypted. Safe to commit.[0m"
 else
-echo "[38;5;208mVault not encrypted! Run 'make encrypt' and try again.[0m"
+echo "[38;5;208mVault not encrypted! Run 'scripts/lock_vault.sh' and try again.[0m"
 exit 1
 fi
 EOT
